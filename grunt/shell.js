28b4e6124b5c58ab78a0e0fd500017f1b916a283
Alexander Feiglstorfer <delooks@gmail.com>
2015-08-12 16:43:41 +0200

    update package

--- grunt/shell.js
+++ grunt/shell.js
@@ -1,9 +1,9 @@
 var ftpCommand = function(folder, ip) {
-	return 'lftp -u "dd,dd" -e "mirror --exclude \'cache/\' --exclude app.db --exclude composer.json --exclude composer.lock -R -L -e webapp /srv/www/doka/'+folder+'/revolution/webapp;mirror -R -L -e public /srv/www/doka/'+folder+'/revolution/public;mirror -R -L -e data /srv/www/doka/'+folder+'/revolution/data;put -O /srv/www/doka/'+folder+'/revolution index.php; bye" sftp://'+ip
+	return 'lftp'
 }
 
 var ftpCommandCms = function(ip) {
-	return 'lftp -u "dd,dd" -e "mirror -R -L -e public /Node/var/httpd/htdocs/revolution/public;mirror -R -L -e data /Node/var/httpd/htdocs/revolution/data;mirror -R -L -e cms /Node/var/httpd/htdocs/revolution/cms; bye" sftp://'+ip
+	return 'lftp'
 }
 
 module.exports = {
@@ -13,15 +13,15 @@ module.exports = {
 		stdin: true
 	},
 	syncDevelopmentWeb: {
-		command: ftpCommand('test.example.com', '62.99.231.27')
+		command: ftpCommand('test.example.com', '10.0.0.1')
 	},
 	syncDevelopmentCms: {
-		command: ftpCommandCms('10.1.1.192')
+		command: ftpCommandCms('10.0.0.1')
 	},
 	syncLiveWeb: {
-		command: ftpCommand('live.example.com', '62.99.231.26')
+		command: ftpCommand('live.example.com', '10.0.0.1')
 	},
 	syncLiveCms: {
-		command: ftpCommandCms('10.1.1.193')
+		command: ftpCommandCms('10.0.0.1')
 	}
 }
