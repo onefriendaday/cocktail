b4964dd84582cf5ec98bf8799a102a3cf88f29b6
Alexander Feiglstorfer <delooks@gmail.com>
2015-08-19 21:16:09 +0200

    adds php as grunt

---
+++ grunt/php.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dev: {
+        options: {
+            hostname: '127.0.0.1',
+            port: 8080,
+            router: 'public/index.php',
+            keepalive: true,
+            open: '/de/home',
+            silent: true
+        }
+    }
+}
